e1a4b77bf7d5e68be3301049221454988e4530ea
Junji Zhi <junji@500px.com>
2017-01-17 16:38:42 -0500

    minor fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-echo never | tee /sys/kernel/mm/transparent_hugepage/enabled
-echo never | tee /sys/kernel/mm/transparent_hugepage/defrag
+echo "Disable Transparent Huge Paging"
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
 
 #jps_tokudb_admin --enable -uroot -pdbpas
 #service mysql restart
 #mysql --user=root --password=dbpass -e "update mysql.user set password=null where User='root';";  exit 0
 #mysql --user=root --password=dbpass -e "flush privileges;";  exit 0
 
-service mysql start && /bin/bash
+service mysql start && tail -F /var/log/mysql/error.log
